62271f230f0e7577b9010b4837b353880021c135
James Richardson <76536315+Icyfiremario@users.noreply.github.com>
2026-07-30 17:44:55 -0600

    Begin reimplementing the swapchain

---
+++ BasedVK/BVKSwapChain.cpp
@@ -0,0 +1,13 @@
+#include "BVKSwapChain.h"
+
+BVKSwapChain::BVKSwapChain(BVKDevice& deviceRef, VkExtent2D windowExtent)
+{
+}
+
+BVKSwapChain::BVKSwapChain(BVKDevice& deviceRef, VkExtent2D windowExtent, std::shared_ptr<BVKSwapChain> previous)
+{
+}
+
+BVKSwapChain::~BVKSwapChain()
+{
+}
---
+++ BasedVK/BVKSwapChain.h
@@ -0,0 +1,83 @@
+#ifndef BASEDENGINE_BVKSWAPCHAIN_H
+#define BASEDENGINE_BVKSWAPCHAIN_H
+
+// STD
+#include <memory>
+#include <vector>
+#include <array>
+#include <set>
+
+// Vulkan
+#include <vulkan/vulkan.h>
+
+// BasedVK
+#include "BVKDevice.h"
+
+class BVKSwapChain
+{
+public:
+    static constexpr int MAX_FRAMES_IN_FLIGHT = 2;
+
+    BVKSwapChain(BVKDevice &deviceRef, VkExtent2D windowExtent);
+    BVKSwapChain(BVKDevice &deviceRef, VkExtent2D windowExtent, std::shared_ptr<BVKSwapChain> previous);
+    ~BVKSwapChain();
+
+    BVKSwapChain(const BVKSwapChain &) = delete;
+    BVKSwapChain &operator=(const BVKSwapChain &) = delete;
+
+    [[nodiscard]] VkFramebuffer getFramebuffer(const int index) const { return swapChainFramebuffers[index]; }
+    [[nodiscard]] VkRenderPass getRenderPass() const { return renderPass; }
+    [[nodiscard]] VkImageView getImageView(const int index) const { return swapChainImageViews[index]; }
+    [[nodiscard]] size_t imageCount() const { return swapChainImages.size(); }
+    [[nodiscard]] VkFormat getSwapChainImageFormat() const { return swapChainImageFormat; }
+
+private:
+#ifdef DEBUG
+    const bool logSwapchain = true;
+#else
+    const bool logSwapchain = false;
+#endif
+
+    BVKDevice &device;
+
+    VkFormat swapChainImageFormat;
+    VkFormat swapChainDepthFormat;
+    VkExtent2D swapChainExtent;
+    VkExtent2D windowExtent;
+
+    std::vector<VkFramebuffer> swapChainFramebuffers;
+    VkRenderPass renderPass;
+
+    std::vector<VkImage> depthImages;
+    std::vector<VkDeviceMemory> depthImageMemories;
+    std::vector<VkImageView> depthImageViews;
+    std::vector<VkImage> swapChainImages;
+    std::vector<VkImageView> swapChainImageViews;
+
+    VkSwapchainKHR swapChain;
+
+    std::vector<VkSemaphore> imageAvailableSemaphores;
+    std::vector<VkSemaphore> renderFinishedSemaphores;
+    std::vector<VkFence> inFlightFences;
+    std::vector<VkFence> imagesInFlight;
+
+    std::shared_ptr<BVKSwapChain> oldSwapChain;
+
+    size_t currentFrame = 0;
+
+    void createSwapChain();
+    void createImageViews();
+    void createDepthResources();
+    void createRenderPass();
+    void createFramebuffers();
+    void createSyncObjects();
+
+    void init();
+
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats);
+    VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR> &availablePresentModes);
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities);
+};
+
+
+#endif //BASEDENGINE_BVKSWAPCHAIN_H
